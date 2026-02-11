04b41feea03b9aa0882e44948d7f03d78d0571ff
Changheon Han <133609341+hanprd@users.noreply.github.com>
2026-02-11 09:46:13 -0500

    Enhance data_downloader.sh to extract and organize data
    Added Python script to extract zip files and move contents.

--- data_downloader.sh
+++ data_downloader.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 kaggle datasets download changheonhan/dinos-diverse-industrial-operation-sounds
+
+python3 -c "
+import zipfile, glob, os, shutil
+for f in glob.glob('*.zip'):
+    with zipfile.ZipFile(f, 'r') as z:
+        z.extractall()
+    os.remove(f)
+
+if os.path.isdir('DINOS'):
+    for item in os.listdir('DINOS'):
+        src = os.path.join('DINOS', item)
+        dst = os.path.join('.', item)
+        shutil.move(src, dst)
+    os.rmdir('DINOS')
+"
